04d5865966f0d0fe054f67ff8a2d0047ea07d243
Hadi mk <hadimk04@gmail.com>
2024-08-22 11:55:43 +0530

    dashboard bug

--- src/pages/teacher/TeacherHome.jsx
+++ src/pages/teacher/TeacherHome.jsx
@@ -2,7 +2,15 @@ import React, { useEffect, useState } from "react";
 import axios from "../../services/axiosService";
 import TeacherSideBar from "../../components/teacher/TeacherSideBar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUsers, faUser, faClipboardList, faTasks, faUserGraduate, faChalkboardTeacher, faBook, } from "@fortawesome/free-solid-svg-icons"; // Import required icons
+import {
+  faUsers,
+  faUser,
+  faClipboardList,
+  faTasks,
+  faUserGraduate,
+  faChalkboardTeacher,
+  faBook,
+} from "@fortawesome/free-solid-svg-icons"; // Import required icons
 import { useSelector } from "react-redux";
 
 function TeacherHome() {
@@ -11,7 +19,7 @@ function TeacherHome() {
   const [totalBatchStudents, setTotalBatchStudents] = useState(0);
   const [presentStudents, setPresentStudents] = useState(0);
   const [absentStudents, setAbsentStudents] = useState(0);
-  
+
   useEffect(() => {
     fetchData();
   }, []);
@@ -19,16 +27,22 @@ function TeacherHome() {
   const fetchData = async () => {
     try {
       // Fetch total number of students
-      const teacherResponse = await axios.get(`/teacher/get-teacher/${currentUser._id}`);
+      const teacherResponse = await axios.get(
+        `/teacher/get-teacher/${currentUser._id}`
+      );
       const teacherData = teacherResponse.data.teacher;
       setTeacher(teacherData);
       const teacherWithBatchAndStudents = teacherResponse.data.teacher;
-      setTotalBatchStudents(teacherWithBatchAndStudents.batchId.students.length);
-  
+      setTotalBatchStudents(
+        teacherWithBatchAndStudents.batchId.students.length
+      );
+
       // Fetch attendance data
-      const attendanceResponse = await axios.get(`/teacher/get-attendances/${teacherData.batchId._id}`);
+      const attendanceResponse = await axios.get(
+        `/teacher/get-attendances/${teacherData.batchId._id}`
+      );
       const attendanceData = attendanceResponse.data;
-  
+
       // Calculate total attendance, present students, and absent students
       let totalAttendance = 0;
       let presentCount = 0;
@@ -38,56 +52,75 @@ function TeacherHome() {
         presentCount += attendance.present.length;
         absentCount += attendance.absent.length;
       });
-  
+
       setPresentStudents(presentCount);
       setAbsentStudents(absentCount);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   };
-  
 
   return (
     <div className="flex">
       <TeacherSideBar />
       <div className="container mx-auto px-4 py-8 ml-56">
-        <h2 className="text-3xl font-semibold mb-6 text-white">Teacher Dashboard</h2>
+        <h2 className="text-3xl font-semibold mb-6 text-white">
+          Teacher Dashboard
+        </h2>
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 text-center">
           <div className="bg-gray-200 p-6 rounded-md">
             <h3 className="text-2xl font-semibold mb-3 text-gray-700">
-              <FontAwesomeIcon icon={faUsers} className="mr-2" /> Total Batch Students
+              <FontAwesomeIcon icon={faUsers} className="mr-2" /> Total Batch
+              Students
             </h3>
-            <p className="text-5xl font-bold text-gray-900">{totalBatchStudents}</p>
+            <p className="text-5xl font-bold text-gray-900">
+              {totalBatchStudents}
+            </p>
           </div>
           <div className="bg-gray-200 p-6 rounded-md">
             <h3 className="text-2xl font-semibold mb-3 text-gray-700">
-              <FontAwesomeIcon icon={faClipboardList} className="mr-2" /> Total Attendance taken
+              <FontAwesomeIcon icon={faClipboardList} className="mr-2" /> Total
+              Attendance taken
             </h3>
-            <p className="text-5xl font-bold text-gray-900">{(presentStudents + absentStudents)/totalBatchStudents}</p>
+            <p className="text-5xl font-bold text-gray-900">
+              {Math.round(
+                (presentStudents + absentStudents) / totalBatchStudents
+              )}
+            </p>
           </div>
           <div className="bg-gray-200 p-6 rounded-md">
             <h3 className="text-2xl font-semibold mb-3 text-gray-700">
-              <FontAwesomeIcon icon={faTasks} className="mr-2" /> Present Students
+              <FontAwesomeIcon icon={faTasks} className="mr-2" /> Present
+              Students
             </h3>
-            <p className="text-5xl font-bold text-gray-900">{presentStudents}</p>
+            <p className="text-5xl font-bold text-gray-900">
+              {presentStudents}
+            </p>
           </div>
           <div className="bg-gray-200 p-6 rounded-md">
             <h3 className="text-2xl font-semibold mb-3 text-gray-700">
-              <FontAwesomeIcon icon={faUserGraduate} className="mr-2" /> Absent Students
+              <FontAwesomeIcon icon={faUserGraduate} className="mr-2" /> Absent
+              Students
             </h3>
             <p className="text-5xl font-bold text-gray-900">{absentStudents}</p>
           </div>
           <div className="bg-gray-200 p-6 rounded-md">
             <h3 className="text-2xl font-semibold mb-3 text-gray-700">
-            <FontAwesomeIcon icon={faChalkboardTeacher} className="mr-2" />Assigned Batch 
+              <FontAwesomeIcon icon={faChalkboardTeacher} className="mr-2" />
+              Assigned Batch
             </h3>
-            <p className="text-3xl font-bold text-gray-900">{currentUser.batch}</p>
+            <p className="text-3xl font-bold text-gray-900">
+              {currentUser.batch}
+            </p>
           </div>
           <div className="bg-gray-200 p-6 rounded-md">
             <h3 className="text-2xl font-semibold mb-3 text-gray-700">
-            <FontAwesomeIcon icon={faBook} className="mr-2" />Assigned Subject 
+              <FontAwesomeIcon icon={faBook} className="mr-2" />
+              Assigned Subject
             </h3>
-            <p className="text-3xl font-bold text-gray-900">{currentUser.subject}</p>
+            <p className="text-3xl font-bold text-gray-900">
+              {currentUser.subject}
+            </p>
           </div>
         </div>
       </div>
